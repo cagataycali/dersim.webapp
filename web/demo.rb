0c88016b1a0e275e63fba9668cd254acc5d93821
cagataycali <cagataycali@icloud.com>
2017-12-03 22:47:51 +0300

    ✨  Yarn updated

--- web/demo.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-ARGF.each_with_index do |line, idx|
-    print ARGF.filename, ":", idx, ";", line
-end
-
-# print all the lines in every file passed via command line that contains login
-ARGF.each do |line|
-    puts line if line =~ /login/
-end
